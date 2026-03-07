99476396e8b2826cb458d9c6543502c01d9831fe
Matheus Gonzalez <matheusgonzaleze@gmail.com>
2026-03-07 14:02:16 -0300

    Refine prompt for financial transaction categorization
    - Updated prompt to specify that transactions will be provided in a CSV file.
    - Clarified that all transactions correspond to credit card transactions.
    - Added a new category for "Posto de Gasolina".
    - Commented out previous categories related to "Receitas", "Moradia", "Fatura", "Investimento", and "Transferências para terceiros" for potential future use.

--- src/prompt_personal_finance.py
+++ src/prompt_personal_finance.py
@@ -1,9 +1,9 @@
 _PROMPT_TEMPLATE = """
 Você é um analista de dados, trabalhando em um projeto de limpeza de dados.
 Seu trabalho é escolher uma categoria adequada para cada lançamento financeiro
-que vou te enviar.
+que vou te enviar em um arquivo csv.
 
-Todas as transações são transações financeiras de uma pessoa física.
+Todas as transações são transações financeiras de uma pessoa física e correspondem a transações de um cartão de crédito.
 
 Escolha uma dentre as seguintes categorias:
 - Assinaturas e Serviços: Sempre que tiver "Cinemark","Wellhub","Netflix","Facebk","Openai","Ebn*Spotify" no nome da transação
@@ -13,15 +13,17 @@ Escolha uma dentre as seguintes categorias:
 - Compras Parceladas: Sempre que tiver "Parcela" no nome da transação
 - Saúde: Sempre que tiver "Drogasil" no nome da transação
 - Restaurantes: Sempre que tiver "Black Dogs" no nome da transação
-- Receitas: Sempre que tiver "Transferência recebida pelo Pix - MATHEUS GONZALEZ EUGENIO" no nome da transação
 - Telefone: Sempre que tiver "Recarga de celular" no nome da transação
-- Moradia: Sempre que tiver "ELETROPAULO","Pagamento de boleto efetuado - PJBANK PAGAMENTOS S A","CONDOMINIO EDIFICIO" no nome da transação
-- Fatura: Sempre que tiver "Pagamento de fatura","Pagamento recebido" no nome da transação
-- Investimento: Sempre que tiver "RDB","NuInvest" no nome da transação
-- Transferências para terceiros
+- Posto de Gasolina
 
 Escolha a categoria deste item:
 {text}
 
 Responda apenas com a categoria correspondente sem pontuação.
 """
+
+# - Receitas: Sempre que tiver "Transferência recebida pelo Pix - MATHEUS GONZALEZ EUGENIO" no nome da transação
+# - Moradia: Sempre que tiver "ELETROPAULO","Pagamento de boleto efetuado - PJBANK PAGAMENTOS S A","CONDOMINIO EDIFICIO" no nome da transação
+# - Fatura: Sempre que tiver "Pagamento de fatura","Pagamento recebido" no nome da transação
+# - Investimento: Sempre que tiver "RDB","NuInvest" no nome da transação
+# - Transferências para terceiros
